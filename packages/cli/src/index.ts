8290573c93596deebfe19f94692522e901410d29
yusuke_senaga <yusuke_senaga@partners.tailor.tech>
2025-12-25 05:02:00 +0900

    chore: 単一パッケージ移行に合わせてワークスペースと旧構成を整理

--- packages/cli/src/index.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { run } from "./main.js";
-
-run(process.argv.slice(2), process.cwd()).catch((err) => {
-  process.stderr.write(`${err instanceof Error ? err.message : String(err)}\n`);
-  process.exitCode = 1;
-});
